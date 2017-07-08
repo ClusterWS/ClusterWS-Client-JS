13134cec30168eb37b4031cc1971dde19554f775
Dmitrii Goriunov <goriunovd@gmail.com>
2017-07-09 03:40:41 +1200

    new: Make git version be the same as package json vestion

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var jeditor = require("gulp-json-editor");
+var exec = require('child_process').exec;
 var tsProject = ts.createProject("tsconfig.json");
 
 
@@ -15,11 +16,17 @@ gulp.task("clone", function () {
         .pipe(jeditor(function(json) {
             delete json['devDependencies'];
             delete json['scripts'];
+            var execCommand = "git tag -a "+ json.version + " -m \"Update version\"";
+            exec(execCommand, function(err, stdout, stderr){
+                console.log(err);
+                console.log(stdout);
+                console.log(stderr);
+            });
             return json;
         }))
         .pipe(gulp.dest("dist/npm"));
     return gulp.src('./README.md')
-        .pipe(gulp.dest("dist/npm"))
+        .pipe(gulp.dest("dist/npm"));
 });
 
 
